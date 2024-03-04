f3446d042bc1fbb271dadc8001a44923d565fda4
Sara Di Luca <150133964+SaraDL92@users.noreply.github.com>
2024-03-04 17:47:53 +0100

    Repo and Service GreenSection CRUD created

--- src/main/java/com/example/val_ex_training_spring/entity/GreenSectionEntity.java
+++ src/main/java/com/example/val_ex_training_spring/entity/GreenSectionEntity.java
@@ -13,10 +13,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Data
+
 @NoArgsConstructor
 @Table(name="green_section")
 	public class GreenSectionEntity {
---
+++ src/main/java/com/example/val_ex_training_spring/service/GreenSectionService.java
@@ -0,0 +1,85 @@
+package com.example.val_ex_training_spring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.val_ex_training_spring.entity.GreenSectionEntity;
+import com.example.val_ex_training_spring.entity.ParagraphEntity;
+import com.example.val_ex_training_spring.entity.UserEntity;
+import com.example.val_ex_training_spring.repository.GreenSectionRepository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Service
+public class GreenSectionService {
+private final GreenSectionRepository greenSectionRepo;
+
+@Autowired
+public GreenSectionService(GreenSectionRepository greensectionrepo) 
+{
+	this.greenSectionRepo=greensectionrepo;
+	
+}
+public boolean saveEntity(GreenSectionEntity gse) 
+{
+	GreenSectionEntity savedGS=	greenSectionRepo.save(gse);
+	Long id=savedGS.getIdGreenSection();
+	Optional<GreenSectionEntity> greensectionfounded=greenSectionRepo.findById(id);
+	List<GreenSectionEntity>greensectionlist=greenSectionRepo.findAll();
+	if(greensectionlist.contains(greensectionfounded)) {
+		
+	return true;}
+	return false;
+	
+
+	
+}
+public GreenSectionEntity findGreenSectionById(Long id) throws NotFoundException
+{GreenSectionEntity gs=greenSectionRepo.findById(id).orElseThrow(() -> new NotFoundException());
+
+return gs;
+
+	}
+public List <GreenSectionEntity> findAllGreenSection()
+{
+	List<GreenSectionEntity>greensectlist=greenSectionRepo.findAll();
+	return greensectlist;
+}
+
+public boolean deleteGreenSection(GreenSectionEntity gs) {
+	greenSectionRepo.delete(gs);
+	Long id=gs.getIdGreenSection();
+	List <GreenSectionEntity>lista=greenSectionRepo.findAll();
+	if(lista.contains(id)) {
+		return false;
+	}return true;
+}
+
+public boolean updateGreenSection(Long idGreenSection,String title,UserEntity idUser)  throws NotFoundException
+{
+GreenSectionEntity gs=this.findGreenSectionById(idGreenSection);
+if(greenSectionRepo.findAll().contains(gs)) {
+gs.setIdUser(idUser);
+gs.setTitle(title);}
+
+GreenSectionEntity gssaved=greenSectionRepo.save(gs);
+if(gssaved!=null) {return true;}return false;
+
+
+	
+	 
+	
+	 
+}
+
+
+
+}
