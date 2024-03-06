3ebca1cf492c1e9e86698ede3bef2a9ba7ab03be
NBK433LEH3QET4X\User <davidefiore92@virgilio.it>
2024-03-06 10:08:03 +0100

    Paragraph repository and service completed

--- src/main/java/com/example/val_ex_training_spring/repository/ParagraphRepository.java
+++ src/main/java/com/example/val_ex_training_spring/repository/ParagraphRepository.java
@@ -1,20 +1,23 @@
 package com.example.val_ex_training_spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.val_ex_training_spring.entity.GreenSectionEntity;
 import com.example.val_ex_training_spring.entity.ParagraphEntity;
 
-import jakarta.transaction.Transactional;
-
 public interface ParagraphRepository extends JpaRepository<ParagraphEntity, Long>{
 	
-	@Transactional
-	@Modifying
-	@Query("UPDATE ParagraphEntity p SET title = :title, description = :description, idGreenSection = :idGreenSection WHERE idparagraphs = :idparagraphs")
-    int updateParagraph(@Param("title") String title, @Param("description") String description, @Param("idGreenSection") GreenSectionEntity idGreenSection, @Param("idparagraphs") Long idParagraph);
+//	@Transactional
+//	@Modifying
+//	@Query("UPDATE ParagraphEntity p SET title = :title, description = :description, idGreenSection = :idGreenSection WHERE idparagraphs = :idparagraphs")
+//    int updateParagraph(@Param("title") String title, @Param("description") String description, @Param("idGreenSection") GreenSectionEntity idGreenSection, @Param("idparagraphs") Long idParagraph);
 
+	List<ParagraphEntity> findByTitle(String title);
+	
+	@Query("SELECT p FROM ParagraphEntity p WHERE idGreenSection = :idGreenSection")
+	List<ParagraphEntity> findByIdGreenSection(@Param("idGreenSection")GreenSectionEntity idGreenSection);
 }
